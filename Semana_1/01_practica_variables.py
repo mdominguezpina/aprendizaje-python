319bef348e871cb22a2dcc9a56339538cb941ff9
mdominguezpina <mdominguezpina.dev@gmail.com>
2025-03-13 19:46:29 +0100

    Semana 1 - Ejercicio 1: Variables

---
+++ Semana_1/01_practica_variables.py
@@ -0,0 +1,21 @@
+nombre_entrante="Sopa de trufas"
+nombre_principal="Filete Mignon con salsa de vino tinto"
+nombre_postre="Tiramisú casero"
+
+precio_entrante=12
+precio_principal=30
+precio_postre=8
+
+print("Menú del Día - Restaurante Gourmet")
+print("--Entrante--")
+print(nombre_entrante)
+print("Precio:")
+print(f"{precio_entrante} euros")
+print("--Plato Principal--")
+print(nombre_principal)
+print("Precio:")
+print(f"{precio_principal} euros")
+print("--Postre--")
+print(nombre_postre)
+print("Precio:")
+print(f"{precio_postre} euros") 
